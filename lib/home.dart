4f6673f66c05cadf73f4764bdaa33d4e10523bf7
Hussein Ameen <husseinameen99@gmail.com>
2025-09-14 13:47:28 +0300

    this edit until i make video

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:runway_fashion/widgets/custom_appbar.dart';
-class Home extends StatelessWidget {
+import 'package:video_player/video_player.dart';
+class Home extends StatefulWidget {
   const Home({super.key});
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  late VideoPlayerController _controller;
+  @override
+  void initState() {
+    _controller=VideoPlayerController.asset(
+        'assets/video/267241_small.mp4',
+    )..initialize().then((_) {
+      // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+      setState(() {
+        _controller.play();
+      });
+    });
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,6 +29,14 @@ class Home extends StatelessWidget {
           title: 'Runway',
           suffix: 'assets/svgs/solar_bell-line-duotone.png',
           Prefix: 'assets/svgs/menu.svg'),
+
+    body: Column(
+      children: [
+               AspectRatio(
+                   aspectRatio: _controller.value.aspectRatio,
+                   child: VideoPlayer(_controller)),
+      ],
+    ),
     );
   }
 }
---
+++ lib/widgets/video_home.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoApp extends StatefulWidget {
+  const VideoApp({super.key});
+
+  @override
+  _VideoAppState createState() => _VideoAppState();
+}
+
+class _VideoAppState extends State<VideoApp> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset('assets/video/267241_small.mp4')
+      ..initialize().then((_) {
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        setState(() {});
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }}
